6937ba5f2ab617c9611dd69e4cfae7a911a3a96c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-17 14:19:17 +0800

    Update yourpropfirm-switch-variant.php

--- yourpropfirm-switch-variant.php
+++ yourpropfirm-switch-variant.php
@@ -50,15 +50,18 @@ class YourPropFirm_Switch_Variant {
 
         $variations = $product->get_available_variations();
         $attributes = $product->get_variation_attributes();
+        $default_attributes = $product->get_default_attributes();
 
         echo '<div id="yourpropfirm-variant-switcher">';
         echo '<h3>Choose Variant</h3>';
         
         foreach ($attributes as $attribute_name => $options) {
+            $default_value = isset($default_attributes[$attribute_name]) ? $default_attributes[$attribute_name] : '';
             echo '<label>' . wc_attribute_label($attribute_name) . '</label>';
             echo '<select class="yourpropfirm-switch" data-attribute="' . esc_attr($attribute_name) . '">';
             foreach ($options as $option) {
-                echo '<option value="' . esc_attr($option) . '">' . esc_html($option) . '</option>';
+                $selected = ($option == $default_value) ? ' selected' : '';
+                echo '<option value="' . esc_attr($option) . '"' . $selected . '>' . esc_html($option) . '</option>';
             }
             echo '</select>';
         }
