6c735e9626b2fb41528ed64ba5bd7a63a69a0fb4
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-17 14:41:37 +0800

    Update switch-variant.js

--- assets/js/switch-variant.js
+++ assets/js/switch-variant.js
@@ -23,13 +23,28 @@
                 },
                 success: function(response) {
                     if (response.success) {
-                        location.reload();
+                        // Update cart fragments without reloading
+                        $(document.body).trigger('wc_fragment_refresh');
+                        // Store selected variation in localStorage
+                        localStorage.setItem('yourpropfirm_selected_variation', JSON.stringify(selectedAttributes));
                     } else {
                         alert(response.data.message);
                     }
                 },
                 complete: function() {
                     $('#yourpropfirm-update-cart').prop('disabled', false).text('Update Cart');
+                }
+            });
+        }
+
+        // Restore last selected variation after refresh
+        let storedVariation = localStorage.getItem('yourpropfirm_selected_variation');
+        if (storedVariation) {
+            storedVariation = JSON.parse(storedVariation);
+            $('.yourpropfirm-switch').each(function() {
+                let attribute = $(this).data('attribute');
+                if (storedVariation[attribute]) {
+                    $(this).val(storedVariation[attribute]);
                 }
             });
         }
